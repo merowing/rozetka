2104797e28268533e9029a22e1c8d45f67b9ac2f
Merowing <elgynart@gmail.com>
2019-02-07 22:56:27 +0200

    add get category block & fix some errorrrs

--- index.user.js
+++ index.user.js
@@ -10,7 +10,8 @@
     let url = window.location.href;
     // we must set parameter modified for the reason that sometimes data change thanks to html.history
     div.querySelector('#parameters-filter-form').setAttribute("modified", "");
-
+    
+    let index = 0;
     for(let i = 0; i < lenItemsLinks; i++) {
         itemsLinksA[i].addEventListener("click", function(e) {
             e.preventDefault();
@@ -42,23 +43,64 @@
         }
         item.querySelector('span').setAttribute('class', classStr);
 
-        let categoryId = getParentId(item).getAttribute('id');
+        //let categoryId = getParentId(item).getAttribute('id');
+        
+        // -- get category block
+        let categoryStr = getCategory(item);
+        
+        let categoryLink = decodeURIComponent(categoryStr.href);
+        let categoryName = categoryStr.name;
 
-        return;
-        categoryId = categoryId.split('_');
-        if(categoryId[1] === 'seller') {
-            switch(categoryId[2]) {
+        let categoryId = [];
+        categoryId[0] = categoryName.split('_')[0];
+        
+        let reg = new RegExp(categoryId[0] + "=([a-z,\\-0-9]+)", "gi");
+        let values = reg.exec(categoryLink)[1];
+
+        if(url.indexOf(categoryId[0]) !== -1) {
+            reg = new RegExp(categoryId[0] + "=([a-z,\\-0-9]+)", "gi");
+            let valuesUrl = reg.exec(url)[1];
+
+            let valuesArr = values.split(',');
+            let valuesUrlArr = valuesUrl.split(',');
+
+            if(active) {
+                categoryId[1] = valuesArr[0];
+            }else {
+                let found = false;
+                for(let q = 0; q < valuesArr.length; q++) {
+                    found = false;
+                    for(let q1 = 0; q1 < valuesUrlArr.length; q1++) {
+                        if(valuesArr[q] == valuesUrlArr[q1]) {
+                            found = true;
+                            break;
+                        }
+
+                        if(!found && q1 == valuesUrlArr.length - 1) {
+                            categoryId[1] = valuesArr[q];
+                        }
+                    }
+                }
+            }
+        }else {
+            categoryId[1] = values;
+        }
+        console.log("category:" + categoryId[1]);
+
+        if(categoryId[0] === 'seller') {
+            switch(categoryId[1]) {
                 case "1":
-                    categoryId[2] = "rozetka";
+                    categoryId[1] = "rozetka";
                     break;
                 case "2":
-                    categoryId[2] = "other";
+                    categoryId[1] = "other";
             }
         }
         
-
         console.log(categoryId);
         
+        // -- end get category block
+
         // url.match(/\/[a-z=0-9;,\-]+\//gi)[1].replace(/\//g,'');
         let str = url.match(/\/[a-z=0-9;,\-]+\//gi);
         let data = [];
@@ -71,15 +113,15 @@
                 let key = data[0];
                 let val = data[1];
 
-                if(key === categoryId[1]) {
+                if(key === categoryId[0]) {
                     same = true;
                     let valArr = val.split(',');
                     if(!active) {
-                        valArr.push(categoryId[2]);
+                        valArr.push(categoryId[1]);
                     }else {
                         if(valArr.length > 1) {
                             for(let l =0; l < valArr.length; l++) {
-                                if(valArr[l] == categoryId[2]) {
+                                if(valArr[l] == categoryId[1]) {
                                     valArr.splice(l, 1);
                                 }
                             }
@@ -96,7 +138,7 @@
             if(!same) {
                 // use slice instead of shift, because shift more slower than slice
                 let categoryIdNew = categoryId.slice(1, categoryId.length); // remove first element, which is not needed
-                str.push(categoryIdNew.join('='));
+                str.push(categoryId.join('='));
             }
             console.log(str);
             
@@ -108,7 +150,7 @@
             });
 
         }else {
-            url += str.join(';') + "/"
+            url += categoryId.join("=") + "/"
         }
         console.log(url);
 
@@ -116,7 +158,7 @@
     }
     modified();
     
-    function getParentId(item) {
+    function getCategory(item) {
         if(item.tagName === "LABEL") {
             return item.querySelector('a');
         }
