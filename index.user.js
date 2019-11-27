1826b96a1d1750db844b8c4dc2d4c1371fb73c29
Merowing <elgynart@gmail.com>
2019-11-27 23:07:04 +0200

    fix select items and url string

--- index.user.js
+++ index.user.js
@@ -1,6 +1,7 @@
 (function(){
     let url = window.location.href;
     let checkItemIds = [];
+    let defaultLen = 0;
 
     // ------------
     let str = document.querySelector("#rz-client-state").innerText;
@@ -58,7 +59,7 @@
     function generation() {
         //return;
         let sidebar = document.querySelector('aside.sidebar');
-        let items = sidebar.querySelectorAll('li.checkbox-filter__item');
+        let items = sidebar.querySelectorAll('li.checkbox-filter__item > a');
 
         // ----------
 
@@ -96,17 +97,76 @@
         let urlStrArr = [];
         let vanillaUrlStr = "";
 
+        // -----------
+        if(!defaultLen) defaultLen = len;
+        
+        let select = document.querySelector('.catalog-settings select');
+        select.onchange = function() {
+            console.log("yes");
+            url = window.location.href;
+        };
+
+        if(url.indexOf("=") === -1) {
+            checkItemIds = [];
+        }
+
+        // -----------
+
         for(let i = 0; i < len; i++) {
             label = items[i].querySelector('label');
 
-            if(label.getAttribute('click')) continue;
-            if(items[i].querySelector('input').checked) {
-                if(checkItemIds.indexOf(i) === -1)
-                checkItemIds.push(i);
+            // ------------
+            /*if(len < defaultLen) {
+                console.log("test");
+                for(let o = 0; o < checkItemIds.length; o++) {
+                    if(label.getAttribute("n") === checkItemIds[o].toString()) {
+                        let cId = +checkItemIds[o] - 1;
+                        console.log("cId: "+cId);
+                        let prevId = items[cId].querySelector("label").getAttribute("n");
+                        if(prevId !== checkItemIds[o].toString() && +prevId !== checkItemIds[o]) {
+                            checkItemIds[o] = +prevId + 1;
+                        }
+                    }
+                }
+            }*/
+
+            // ------------
+
+            // show sidebar if hidden
+            sidebar.querySelector('div').removeAttribute('style');
+
+            // hide empty items --------------
+
+            if(items[i].getAttribute('class').indexOf('disabled') >= 0) {
+                items[i].parentElement.style.display = "none";
             }
+
+            // -------------------------------
+
+            if(label.getAttribute('click')) continue;
+
+            let checkItemParam = items[i].querySelector('label').getAttribute('param');
+            if(checkItemParam) {
+                let checkItemParamStr = checkItemParam.split('=')[1];
+                let indParam = checkItemIds.indexOf(checkItemParamStr);
+                if(items[i].querySelector('input').checked) {
+                    if(indParam === -1) {
+                        checkItemIds.push(checkItemParamStr);
+                    }
+                }
+
+                for(let j = 0; j < checkItemIds.length; j++) {
+                    if(!items[i].querySelector('input').checked && checkItemIds[j] === checkItemParamStr) {
+                        checkItemIds.splice(indParam, 1);
+                        url = window.location.href;
+                    }
+                }
+            }
+
 
             // set label event click on label tags
             label.setAttribute("click",true);
+            //label.setAttribute("n", i);
             label.addEventListener('click', function(e) {
                 e.preventDefault();
                 
@@ -134,11 +194,18 @@
                     urlStrArr = urlStr.split(';');
                 }*/
 
+                let categoryNotFound = true;
+                let urlArr = url.split('/');
+                if(url.indexOf('=') !== -1) {
+                    urlStr = urlArr.splice(-2,1).toString();
+                    urlStrArr = urlStr.split(';');
+                }
+
                 let params = this.getAttribute('param').split('=');
                 let category = params[0];
                 let item = params[1];
                 console.log(category +" "+item);
-                
+
                 //return;
 
                 // remove item from url ---------------------
@@ -218,22 +285,24 @@
                     console.log(newItem);
 
                 }
-                
+                */
                 //if(newItemFound) {
                     for(let i = 0; i < urlStrArr.length; i++) {
-                        if(urlStrArr[i].indexOf(newCategory) >= 0) {
+                        if(urlStrArr[i].indexOf(category) >= 0) {
                             categoryNotFound = false;
                             let urlCategoryArr1 = urlStrArr[i].split('=');
                             let urlCategoryArr2 = urlCategoryArr1[1].split(',');
 
                             if(!input.checked) {
-                                urlCategoryArr2.push(newItem);
-                                checkItemIds.push(indexItem);
+                                urlCategoryArr2.push(item);
+                                //checkItemIds.push(indexItem);
+                                checkItemIds.push(item);
                             }else {
-                                let index = urlCategoryArr2.indexOf(newItem);
+                                let index = urlCategoryArr2.indexOf(item);
                                 urlCategoryArr2.splice(index,1);
 
-                                let indexId = checkItemIds.indexOf(indexItem);
+                                //let indexId = checkItemIds.indexOf(indexItem);
+                                let indexId = checkItemIds.indexOf(item);
                                 checkItemIds.splice(indexId,1);
                             }
                             if(urlCategoryArr2.length !== 0) {
@@ -247,7 +316,10 @@
                         }
                     }
                     if(categoryNotFound) {
-                        urlStrArr.push(newCategory + "=" + newItem);
+                        urlStrArr.push(category + "=" + item);
+                        
+                        //checkItemIds.push(indexItem);
+                        checkItemIds.push(item);
                     }
                 //}
                 console.log(urlStrArr.join(';'));
@@ -261,18 +333,23 @@
                 //}
 
                 console.log(url);
-                */
+                
                 // check items on which we clicked
                 input.checked = (!input.checked) ? true : false;
-                
+
+                console.log(checkItemIds);
                 e.stopPropagation();
             });
         }
 
         for(let i = 0; i < items.length; i++) {
             items[i].querySelector('input').checked = false;
-            if(checkItemIds.indexOf(i) >= 0) {
-                items[i].querySelector('input').checked = true;
+            let checkParam = items[i].querySelector('label').getAttribute('param');
+            if(checkParam) {
+                let checkParamVal = checkParam.split('=')[1];
+                if(checkItemIds.indexOf(checkParamVal) >= 0) {
+                    items[i].querySelector('input').checked = true;
+                }
             }
         }
 
